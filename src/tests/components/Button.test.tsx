8d96ca7894bffafa3373ee97abb111e6e5aaa1a9
Sundeep Charan Ramkumar <sunguru98@yahoo.co.in>
2020-01-19 19:18:25 +0530

    Performed a basic click event simulation of a button

--- src/tests/components/Button.test.tsx
+++ src/tests/components/Button.test.tsx
@@ -5,13 +5,21 @@ import { fetchComponentByAttribute } from '../../utils/index';
 
 describe('Button component tests', () => {
   let component: ShallowWrapper<ButtonProps, null>;
-
+  let onClick: jest.Mock<void, []>;
   beforeEach(() => {
-    component = shallow(<Button onClick={() => {}}>Hello</Button>) as ShallowWrapper<ButtonProps, null>;
+    onClick = jest.fn()
+    component = shallow(<Button onClick={onClick}>Hello</Button>) as ShallowWrapper<ButtonProps, null>;
   });
 
   it('Should render the button itself', () => {
     const wrapper = fetchComponentByAttribute(component, 'button');
     expect(wrapper).toHaveLength(1);
   });
+
+  it('Should perform a dummy click', () => {
+    const wrapper = fetchComponentByAttribute(component, 'button');
+    wrapper.simulate('click')
+    const callbackFunction = onClick.mock.calls
+    expect(callbackFunction).toHaveLength(1)
+  })
 });
